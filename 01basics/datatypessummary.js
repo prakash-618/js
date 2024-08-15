b4e866691712f4be5c85bbff2b3bde083cf0908f
prakash-618 <prakashranjan45890@gmail.com>
2024-08-15 07:08:30 +0000

    done

---
+++ 01basics/datatypessummary.js
@@ -0,0 +1,32 @@
+// primitive(call by value(copied))
+// 7categories
+// string
+// number
+// boolean
+// null
+// Undefined
+// symbol
+// bigint
+
+
+const id= Symbol('123')
+const another=Symbol('123')
+console.log(id===another)
+
+// Non primitive(call by reference(adress copied))
+// array
+// objects
+// Functions
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// stack(Primitive) and heap(non primitive) memory
+// As we know all primitive data types goes in stack and return copied value
+// but non primitve data types data types stored in  heap and give reference in return to which value will
+// change if changed manually
+
+
+
+
+
