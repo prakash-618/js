860fbdedc597f3afe65b4197af4ae60acb607652
prakash-618 <prakashranjan45890@gmail.com>
2024-08-15 05:19:43 +0000

    my commit :qa!

---
+++ 01basics/02datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js code as newer version
+
+
+// alert("hello") we are using node js not browser
+// code readability should be great
+
+
+let name ="Prakash"
+let age=18
+let isLoggedIn=false
+
+console.log(typeof name)
+
+console.log(typeof null)
+
+console.log(typeof undefined)
+
+console.log(name);
+
+
---
+++ 01basics/variables.js
@@ -0,0 +1,12 @@
+const accountId =144553
+let accountEmail = "prakash@gmail.com"
+var accountPassword ="12345"
+accountCity="jaipur"
+
+
+// accountId=2 not possible
+// prefer not to use var because of issue 
+// and functional scope
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
