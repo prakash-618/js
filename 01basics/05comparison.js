3394859af5d67cf638263d290d5fa3f132f4c7f3
prakash-618 <prakashranjan45890@gmail.com>
2024-08-15 06:08:19 +0000

    my commit

---
+++ 01basics/05comparison.js
@@ -0,0 +1,21 @@
+// console.log(2>1)
+
+// console.log(2>=1)
+
+// console.log("2">1)
+// console.log("02">1)
+// when we compare two different data types there is no predictable outpput is their.
+// Hence avoid using this comparison 
+
+// console.log(null>0)
+// console.log(null==0)
+// console.log(null>=0)
+// Again unpredictable result
+
+
+// console.log(undefined==0)
+// console.log(undefined>0)
+// just avoid
+
+console.log("2"===2)
+// strictly check the values and datatypes also
