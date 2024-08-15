e13013cdc9f58d5f6bede7faa0deb9fbb909f6bd
prakash-618 <prakashranjan45890@gmail.com>
2024-08-15 05:10:18 +0000

    This is

---
+++ 01basics/03conversion.js
@@ -0,0 +1,25 @@
+// let score="33abc"
+let score="33"
+
+console.log(typeof(score))
+
+let valueinnumber = Number(score);
+console.log(typeof valueinnumber)
+console.log(valueinnumber)
+
+// "33"=>33
+// "33abc"=>NaN
+// "true"=>1
+// null=>0
+// undefined=>NaN(it is also a type of number datatype)
+
+
+let loggedIn=2
+let check=Boolean(loggedIn)
+console.log(check)
+
+// 1=>true
+// ""=>false
+// "hitesh"=>true
+
+// for string conversion
